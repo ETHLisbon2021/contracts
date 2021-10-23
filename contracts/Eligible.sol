7e4c2e29b3d9dd203bd5cf62a702b6ed4e42cc29
axdvdv <davy42dev@gmail.com>
2021-10-23 17:52:49 +0300

    date => endingAt

--- contracts/Eligible.sol
+++ contracts/Eligible.sol
@@ -12,9 +12,9 @@ contract Eligible is Merkle {
         uint256 totalCap;
         uint256 maxDeposit;
         uint256 totalDeposits;
-        uint256 date;
+        uint256 endingAt;
         uint256 distribution;
-        bytes32 description;
+        bytes32 ipfsHash;
         bytes32 root;
         address payable initiator;
         saleState state;
@@ -44,8 +44,8 @@ contract Eligible is Merkle {
         uint256 totalCap,
         uint256 distribution,
         uint256 maxDeposit,
-        uint256 date,
-        bytes32 description
+        uint256 endingAt,
+        bytes32 ipfsHash
 
     )
         external
@@ -55,8 +55,8 @@ contract Eligible is Merkle {
             maxDeposit    : maxDeposit,
             totalDeposits : uint256(0),
             distribution  : distribution,
-            date          : date,
-            description   : description,
+            endingAt      : endingAt,
+            ipfsHash      : ipfsHash,
             root          : bytes32(0),
             initiator     : payable(msg.sender),
             state         : saleState.Active
@@ -77,7 +77,7 @@ contract Eligible is Merkle {
     {
         Sale storage sale = sales[tokenAddress];
         require(sale.state == saleState.Active, "sale does not exist");
-        require(sale.date < block.timestamp, "");
+        require(sale.endingAt < block.timestamp, "");
 
         sale.root = root;
         sale.state = saleState.Finished;
@@ -85,12 +85,18 @@ contract Eligible is Merkle {
         emit TokenDistributed(tokenAddress, tree, root);
     }
 
-    function deposit(address tokenAddress, address receiver) external payable {
+    function deposit(
+        address tokenAddress,
+        address receiver
+    )
+        external
+        payable
+    {
         Sale storage sale = sales[tokenAddress];
 
         require(sale.state == saleState.Active, "sale does not exist");
         require(msg.value <= sale.maxDeposit, "");
-        require(block.timestamp < sale.date, "");
+        require(block.timestamp < sale.endingAt, "");
         require(deposits[receiver][tokenAddress] > 0, "");
 
         sale.totalDeposits += msg.value;
@@ -99,11 +105,23 @@ contract Eligible is Merkle {
         emit Deposited(tokenAddress, receiver, msg.value);
     }
 
-    function withdraw(address tokenAddress, address receiver) external {
+    function withdraw(
+        address tokenAddress,
+        address receiver
+    )
+        external
+    {
         sales[tokenAddress].initiator.transfer(deposits[receiver][tokenAddress]);
     }
 
-    function claim(uint256 amount, address receiver, address tokenAddress, bytes memory proof) external {
+    function claim(
+        uint256 amount,
+        address receiver,
+        address tokenAddress,
+        bytes memory proof
+    )
+        external
+    {
         bytes32 leaf = keccak256(abi.encodePacked(receiver, amount));
         require(verifyProof(leaf, sales[tokenAddress].root, proof), "The user is not eligible");
 
